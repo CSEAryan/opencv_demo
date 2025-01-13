ee49ea9632e6c3a783e235865e2bc71f81fbc67a
CSEAryan <dipuaryan1111@gmail.com>
2025-01-13 08:32:41 +0545

    face_detection_haarcascade

---
+++ face_detection.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+
+img = cv.imread(r"C:\Users\Aryan Thapa\Downloads\group.jpg")
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#cv.imshow('grayed', gray)
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+#we can tune the minNeighbors parameter to check the facial detection is correct or not
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+
+print(f"Number of faces found = {len(faces_rect)}")
+
+#we can draw a rectangle around the detected face using the rectangle coordinates of the faces
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
+
+cv.imshow('Detected faces', img)
+
+
+
+cv.waitKey(0)
